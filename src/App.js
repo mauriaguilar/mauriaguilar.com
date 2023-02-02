23daadc0a3314ff2c4dcd3d4042e1c0e7858aa17
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2023-02-02 14:26:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>mauriaguilar.com</h2>
+          <h2>Mauricio Aguilar Portfolio</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
