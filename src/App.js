14d9a66e1433b63f7f89a0ed2fce26c997e0fefa
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2023-02-03 17:48:49 -0300

    Styles

--- src/App.js
+++ src/App.js
@@ -29,26 +29,39 @@ class App extends Component {
     return (
       <div className="container App">
 
-        <div className={"row hidden " + (this.state.showStudies ? 'd-block' : 'd-none')} id="studies">
-          <div className='col App-content'>
+        <div className='row'>
+
+
+
+        <div className={"row " + (this.state.showStudies ? 'd-block' : 'd-none')} id="studies">
+          <div className='col col-md-8 App-content'>
             <div className='App-title'>Studies:</div>
             <div className='App-description'>
               <ul>
                 <li>
-                  Computer Engineer<br/>
-                  Universidad Nacional de Córdoba (2008 - 2022)
+                    Computer Engineer<br/>
+                    <small>Universidad Nacional de Córdoba (2008 - 2022)</small>
                 </li>
-                <li>React Js<br/>CoderHouse (07/2021 - 09/2021)</li>
-                <li>IT Essentials<br/>Cisco Networking Academy (11/2011)</li>
-                <li>Computer repairer, ICIR Institute (09/2005)</li>
-                <li>English Level 3 (2020, 2021, 2022)</li>
+                <li>React Js<br/>
+                    <small>CoderHouse (07/2021 - 09/2021)</small>
+                </li>
+                <li>IT Essentials<br/>
+                    <small>Cisco Networking Academy (11/2011)</small>
+                </li>
+                <li>Computer repairer<br/>
+                    <small>ICIR Institute (09/2005)</small>
+                </li>
+                <li>English Level 3 & 4<br/>
+                    <small>GAPS Academy (2020, 2021, 2022)</small>
+                </li>
               </ul>
             </div>
           </div>
+          <div className='col-xs-0 col-md-1 col-lg-2 AppLateral'></div>
         </div>
 
-        <div  className={"row hidden " + (this.state.showPortfolio ? 'd-block' : 'd-none')} id="portfolio">
-            <div className='col App-content'>
+        <div  className={"row " + (this.state.showPortfolio ? 'd-block' : 'd-none')} id="portfolio">
+            <div className='col col-lg-8 App-content'>
               <div className='App-title'>Portfolio:</div>
                 <div className='App-description'>
                   <ul>
@@ -60,46 +73,57 @@ class App extends Component {
           </div>
         </div>
 
-        <div  className={"row hidden " + (this.state.showSkills ? 'd-block' : 'd-none')} id="skills">
-            <div className='col App-content'>
+        <div  className={"row " + (this.state.showSkills ? 'd-block' : 'd-none')} id="skills">
+            <div className='col col-lg-8 App-content'>
               <div className='App-title'>Skills:</div>
                 <div className='App-description'>
                   <ul>
-                    <li>Frontend: ReactJs, NextJs</li>
-                    <li>Backend: Python, Flask, NodeJs, ExpressJs, AWS ApiGateway, AWS Lambda</li>
+                    <li>Backend: Python, Flask, NodeJs, ExpressJs</li>
                     <li>Cloud: AWS EC2, AWS ECR, AWS IoT</li>
                     <li>Tools: Git, GitHub, Docker, Linux, Makefiles, Bash</li>
+                    <li>Frontend: ReactJs, NextJs</li>
                     <li>Test: PyTest</li>
                   </ul>
                 </div>
           </div>
         </div>
 
+      </div>
+
+
+
+
         <div className='row'>
           <div className='col App-navbar'>
 
             <div className='row'>
-              <div className='col App-domain'>
-                mauriaguilar.com
+
+              <div className='col-xs-0 col-md-1 col-lg-2 AppLateral'></div>
+              <div className='col'>
+
+                <div className='row menu'>
+                  <div className='col App-domain'>
+                    mauriaguilar.com
+                  </div>
+                </div>
+
+                <div className="row menu">
+                    <div className="col item" onClick={this.activateStudies}>
+                      <a className="link active" href="#">Studies</a>
+                    </div>
+                    <div className="col item" onClick={this.activatePortfolio}>
+                      <a className="link" href="#">Portfolio</a>
+                    </div>
+                    <div className="col item" onClick={this.activateSkills}>
+                      <a className="link" href="#">Skills</a>
+                    </div>
+                  </div>
+                
               </div>
+              <div className='col-xs-0 col-md-1 col-lg-2 AppLateral'></div>
+
             </div>
 
-            <nav className="row navbar navbar-expand">
-              <div className="col">
-                <ul className="row navbar-nav mx-auto mb-2 mb-lg-0">
-                    <li className="col nav-item">
-                      <a className="nav-link active" href="#" onClick={this.activateStudies}>Studies</a>
-                    </li>
-                    <li className="col nav-item">
-                      <a className="nav-link" href="#" onClick={this.activatePortfolio}>Portfolio</a>
-                    </li>
-                    <li className="col nav-item">
-                      <a className="nav-link" href="#" onClick={this.activateSkills}>Skills</a>
-                    </li>
-                  </ul>
-              </div>
-            </nav>
-            
           </div>
         </div>
 
