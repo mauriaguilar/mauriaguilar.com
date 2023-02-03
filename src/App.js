7ed64ead945ba2a5fd164ab83495b0a525fbd482
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2023-02-03 14:56:09 -0300

    Adding information

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+    // const [showStudies, setShowStudies] = useState(true);
+    // const [showPorfolio, setPortfolio] = useState(False);
+    // const [showSkills, setSkills] = useState(False);
+
+    function activateStudies(){
+      console.log('You clicked submit.');
+    }
+
     return (
       <div className="container App">
 
-        <div className='row'>
+        <div className="row " id="studies">
             <div className='col App-content'>
               <div className='App-title'>Studies:</div>
               <div className='App-description'>
                 <ul>
-                  <li>Computer Engineer</li>
-                  <li>Techinical IT</li>
-                  <li>Computer repairer</li>
-                  <li>English Level 3</li>
+                  <li>Computer Engineer<br/>Universidad Nacional de Córdoba (2008 - 2022)</li>
+                  <li>React Js<br/>CoderHouse (07/2021 - 09/2021)</li>
+                  <li>IT Essentials<br/>Cisco Networking Academy (11/2011)</li>
+                  <li>Computer repairer, ICIR Institute (09/2005)</li>
+                  <li>English Level 3 (2020, 2021, 2022)</li>
                 </ul>
               </div>
           </div>
         </div>
 
-        <div className='row'>
+        <div className='row' id="portfolio">
             <div className='col App-content'>
               <div className='App-title'>Portfolio:</div>
                 <div className='App-description'>
@@ -33,7 +42,7 @@ class App extends Component {
           </div>
         </div>
 
-        <div className='row'>
+        <div className='row' id="skills">
             <div className='col App-content'>
               <div className='App-title'>Skills:</div>
                 <div className='App-description'>
@@ -61,13 +70,13 @@ class App extends Component {
               <div className="col">
                 <ul className="row navbar-nav mx-auto mb-2 mb-lg-0">
                     <li className="col nav-item">
-                      <a className="nav-link active" href="#">Studies</a>
+                      <a className="nav-link active" href="#" onClick={activateStudies}>Studies</a>
                     </li>
                     <li className="col nav-item">
-                      <a className="nav-link" href="#">Portfolio</a>
+                      <a className="nav-link" href="#" onClick="activatePortfolio()">Portfolio</a>
                     </li>
                     <li className="col nav-item">
-                      <a className="nav-link" href="#">Skills</a>
+                      <a className="nav-link" href="#" onClick="activateSkills()">Skills</a>
                     </li>
                   </ul>
               </div>
